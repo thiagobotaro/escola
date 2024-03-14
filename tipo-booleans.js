42fe0cc47fc7d96861355e7d7f83569aa75a976f
thiagopando <thiagopando06@gmail.com>
2024-03-14 10:52:08 -0300

    Tipo booleano

---
+++ tipo-booleans.js
@@ -0,0 +1,14 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+if (estaAprovado){
+    console.log('Parabéns! Boas Festas');
+} else{
+    console.log('REPROVADO! Entre em contato com o coordenador do curso.');
+}
+
+if (estudante === 'Fernando'){
+    console.log(`Olá, ${estudante}`);
+} else{
+    console.log('Quem é você?');
+}
