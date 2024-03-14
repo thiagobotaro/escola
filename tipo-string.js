badd4644e024666dae2b6cf78f3e472180098992
thiagopando <thiagopando06@gmail.com>
2024-03-14 09:49:23 -0300

    Update-14-03

---
+++ tipo-string.js
@@ -0,0 +1,7 @@
+const estudante = "Caroline";
+const docente = 'Ana';
+const cumprimento = "nosso lema é 'estudar bastânte'";
+const citacao = `Ju diz "nosso lema é 'estudar bastânte'"`;
+
+console.log(cumprimento);
+console.log(citacao);
